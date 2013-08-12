b84502d17f88accc74c75dbcda18fdd005eef03b
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-08-12 14:17:00 -0700

    adjusted adjustability in ArithmaHash

--- ArithmaHash.c
+++ ArithmaHash.c
@@ -26,11 +26,18 @@
 //#define LIMIT 16777216
 //#define LIMIT 65536
 
+// Preferably use consecutive primes for first three and size for last
+#define CONSEC_PRIME_ONE 19 // Largest coprime value; default = 19
+#define CONSEC_PRIME_TWO 17 // Second largest coprime value; default = 17
+#define CONSEC_PRIME_THR 13 // Second smallest coprime value; default = 13
+#define SIZE_COPRIME_FOU 11 // Smallest coprime value; max size (use 2 to the power of bits for true use); default = 11
+
+
 int main (void) {
 	unsigned int x;
 	
 	for (x = 0; x < LIMIT; x++) {
-		printf("%u\n", ((((x + 19) * 17) - 13) % 11));
+		printf("%u\n", ((((x + CONSEC_PRIME_ONE) * CONSEC_PRIME_TWO) - CONSEC_PRIME_THR) % SIZE_COPRIME_FOU));
 	}
 
 	return 0;
